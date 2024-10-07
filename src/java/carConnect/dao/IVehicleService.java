ce22f72e32cc8638746923c82a5cd85d172223c5
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2024-10-07 21:07:54 +0530

    IVehicleService.java

--- src/java/carConnect/dao/IVehicleService.java
+++ src/java/carConnect/dao/IVehicleService.java
@@ -1,12 +1,15 @@
 package carConnect.dao;
 
 import carConnect.entity.Vehicle;
+import carConnect.exception.VehicleNotFoundException;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IVehicleService {
-	Vehicle getVehicleByID(int vehicleID);
-    List<Vehicle> getAvailableVehicles();
-    void addVehicle(Vehicle vehicleData);
-    void updateVehicle(Vehicle vehicleData);
-    void removeVehicle(int vehicleID);
+	Vehicle getVehicleByID(int vehicleID) throws SQLException, VehicleNotFoundException;
+    List<Vehicle> getAvailableVehicles() throws SQLException;
+    void addVehicle(Vehicle vehicleData) throws SQLException;
+    void updateVehicle(Vehicle vehicleData) throws SQLException, VehicleNotFoundException;
+    void removeVehicle(int vehicleID) throws SQLException, VehicleNotFoundException;
 }
