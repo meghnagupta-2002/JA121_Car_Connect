6450de69493268acbe5bdbcf2ad852da16d43a9f
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2024-10-07 21:06:28 +0530

    IReservationService.java

--- src/java/carConnect/dao/IReservationService.java
+++ src/java/carConnect/dao/IReservationService.java
@@ -1,12 +1,15 @@
 package carConnect.dao;
 
 import carConnect.entity.Reservation;
+import carConnect.exception.ReservationException;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IReservationService {
-	Reservation getReservationByID(int reservationID);
-    List<Reservation> getReservationsByCustomerID(int customerID);
-    void createReservation(Reservation reservationData);
-    void updateReservation(Reservation reservationData);
-    void cancelReservation(int reservationID);
+	Reservation getReservationByID(int reservationID) throws SQLException, ReservationException;
+    List<Reservation> getReservationsByCustomerID(int customerID) throws SQLException, ReservationException;
+    void createReservation(Reservation reservationData) throws SQLException, ReservationException;
+    void updateReservation(Reservation reservationData) throws SQLException;
+    void cancelReservation(int reservationID) throws SQLException, ReservationException;
 }
