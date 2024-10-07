b428fef40e8ad5ef9637169e419f91ac4805f4e5
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2024-10-07 18:18:21 +0530

    IReservationService.java

--- src/java/carConnect/dao/IReservationService.java
+++ src/java/carConnect/dao/IReservationService.java
@@ -1,12 +1,12 @@
-package java.carConnect.dao;
+package carConnect.dao;
 
-import java.carConnect.entity.Reservation;
+import carConnect.entity.Reservation;
 import java.util.List;
 
 public interface IReservationService {
-	Reservation getReservationById(int reservationId);
-    List<Reservation> getReservationsByCustomerId(int customerId);
+	Reservation getReservationByID(int reservationID);
+    List<Reservation> getReservationsByCustomerID(int customerID);
     void createReservation(Reservation reservationData);
     void updateReservation(Reservation reservationData);
-    void cancelReservation(int reservationId);
+    void cancelReservation(int reservationID);
 }
