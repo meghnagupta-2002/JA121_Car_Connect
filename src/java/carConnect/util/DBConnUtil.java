7689ce86ee31ad6f262c0dc419f7ab0a42023703
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2024-10-07 17:49:40 +0530

    DBConnUtil.java

--- src/java/carConnect/util/DBConnUtil.java
+++ src/java/carConnect/util/DBConnUtil.java
@@ -1,5 +1,24 @@
 package java.carConnect.util;
 
+import java.sql.*;
+
 public class DBConnUtil {
+
+	public static Connection getConnection(String connectionString) throws SQLException{
+	    Connection connection=null;    
+	            try {
+	                Class.forName("com.mysql.cj.jdbc.Driver");
+	                //System.out.println("Driver Loaded");
+	                connection = DriverManager.getConnection(connectionString);
+	                //System.out.println("Database Connection Established.");
+	            } catch (SQLException err) {
+	                err.printStackTrace();
+	                throw new SQLException("Failed to Establish Connection.", err);
+	            } catch (ClassNotFoundException err) {
+	                err.printStackTrace();
+	                throw new RuntimeException("Driver Loading Failed.", err); 
+	            }
+	        return connection;
+	    }
 
 }
