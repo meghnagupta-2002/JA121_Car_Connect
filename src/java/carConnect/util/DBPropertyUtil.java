7ad3b06c2280f4dc90b674fb5a18633f9aa25e83
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2024-10-07 17:58:43 +0530

    DBPropertyUtil.java

--- src/java/carConnect/util/DBPropertyUtil.java
+++ src/java/carConnect/util/DBPropertyUtil.java
@@ -1,5 +1,27 @@
-package java.carConnect.util;
+package carConnect.util;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DBPropertyUtil {
-
+	public static String getConnectionString(String propertyFileName){
+		String connectionString= null;
+		Properties propertyObject= new Properties();
+		//Created a stream connected to Properties file
+		try(FileInputStream fis= new FileInputStream(propertyFileName))
+		{
+			//try with resources
+			propertyObject.load(fis);
+			//created connection string
+			connectionString= propertyObject.getProperty("db.url")+"?user="+
+								propertyObject.getProperty("db.username")+
+								"&password="+propertyObject.getProperty("db.password");
+			
+		}catch(IOException err)
+		{
+			err.printStackTrace();
+			System.out.println("Error: Not able to connect with Properties file");
+		}
+		return connectionString;
+	}
 }
