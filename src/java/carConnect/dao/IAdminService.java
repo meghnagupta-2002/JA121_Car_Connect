e70a78ef53ff2c432aa10c023c8ce85c875a8be9
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2024-10-08 00:07:10 +0530

    IAdminService.java

--- src/java/carConnect/dao/IAdminService.java
+++ src/java/carConnect/dao/IAdminService.java
@@ -1,12 +1,15 @@
 package carConnect.dao;
 
+import java.sql.SQLException;
+
 import carConnect.entity.Admin;
+import carConnect.exception.AdminNotFoundException;
 
 public interface IAdminService {
 	//Declaration of Non-Implemented Methods
-	Admin getAdminByID(int adminID);
-    Admin getAdminByUsername(String username);
-    void registerAdmin(Admin admin);
-    void updateAdmin(Admin admin);
-    void deleteAdmin(int adminID);
+	Admin getAdminByID(int adminID) throws SQLException, AdminNotFoundException;
+    Admin getAdminByUsername(String username) throws SQLException, AdminNotFoundException;
+    void registerAdmin(Admin admin) throws SQLException;
+    void updateAdmin(Admin admin) throws SQLException, AdminNotFoundException;
+    void deleteAdmin(int adminID) throws SQLException, AdminNotFoundException;
 }
